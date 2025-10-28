60730726cada8d42d0c2fde9908d2e091ef655c4
Marvin Preuss <marvin@xsteadfastx.org>
2025-10-28 10:52:12 +0100

    feat(i3): adds i3auto

--- home-manager/modules/i3/default.nix
+++ home-manager/modules/i3/default.nix
@@ -1,7 +1,26 @@
-{ lib, nixosConfig, ... }:
+{
+  lib,
+  nixosConfig,
+  pkgs,
+  ...
+}:
 let
   cfg = nixosConfig.xsfx;
 in
 lib.mkIf cfg.x11 {
   xdg.configFile."i3/config".source = ./config;
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "i3auto" ''
+      set -e
+      ${pkgs.xorg.xsetroot}/bin/xsetroot -solid "#282a36"
+      ${pkgs.dunst}/bin/dunst &
+      ${pkgs.networkmanagerapplet}/bin/nm-applet &
+      setxkbmap -option "caps:super"
+      ${pkgs.blueman}/bin/blueman-applet &
+      ${pkgs.autorandr}/bin/autorandr -c
+      xset s 60 60
+      ${pkgs.flameshot}/bin/flameshot &
+    '')
+  ];
 }
