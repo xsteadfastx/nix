7afe2dc0deecd43142f1d7dd95bf92cdf82d3fcb
Marvin Preuss <marvin@xsteadfastx.org>
2025-11-18 08:21:54 +0100

    feat: adds helper script for yt

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -55,6 +55,15 @@ in
 
     # download stuff
     yt-dlp
+    (pkgs.writeShellScriptBin "yt-dlp-album" ''
+      set -euo pipefail
+      if [ "$#" -ne 1 ]; then
+        echo "Error: One argument needed (URL)."
+        echo "Usage: yt-dlp-album <URL>"
+        exit 1
+      fi
+      ${yt-dlp}/bin/yt-dlp -x --audio-format mp3 --audio-quality 0 -i -o "%(album)s/%(title)s-%(id)s.%(ext)s" --cookies-from-browser chromium $1
+    '')
 
     # backup
     restic
