4884926d8690f6caf3ae43619e7a00316f3b7d37
Marvin Preuss <marvin@xsteadfastx.org>
2026-01-07 12:28:37 +0100

    fix(vim): warning about params

--- home-manager/modules/neovim/augroups.nix
+++ home-manager/modules/neovim/augroups.nix
@@ -23,7 +23,9 @@ in
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*.go",
         callback = function()
-          local params = vim.lsp.util.make_range_params()
+          local client = vim.lsp.get_clients({ bufnr = 0 })[1]
+          local encoding = client and client.offset_encoding or "utf-16"
+          local params = vim.lsp.util.make_range_params(0, encoding)
           params.context = { only = { "source.organizeImports" } }
           -- buf_request_sync defaults to a 1000ms timeout. Depending on your
           -- machine and codebase, you may want longer. Add an additional
