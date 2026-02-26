1f6f1d00a88b198d02eda808f81fe5e2bd4a5f38
Marvin Preuss <marvin@xsteadfastx.org>
2026-02-26 09:24:44 +0100

    style: formatting

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -61,9 +61,9 @@ in
     (pkgs.writeShellScriptBin "yt-dlp-album" ''
       set -euo pipefail
       if [ "$#" -ne 1 ]; then
-        echo "Error: One argument needed (URL)."
-        echo "Usage: yt-dlp-album <URL>"
-        exit 1
+      	echo "Error: One argument needed (URL)."
+      	echo "Usage: yt-dlp-album <URL>"
+      	exit 1
       fi
       ${yt-dlp}/bin/yt-dlp -x --audio-format mp3 --audio-quality 0 -i -o "%(album)s/%(title)s-%(id)s.%(ext)s" --cookies-from-browser chromium $1
     '')
@@ -100,11 +100,11 @@ in
 
       ${tmux}/bin/tmux rename-window "wobcom-vpn"
       sudo ${openfortivpn}/bin/openfortivpn \
-      vpn.wobcom.de \
-      --trusted-cert 7a3f29e18c303c26080671cd1c0925ba2ae7c229c50eef6222d6f1453596e88d \
-      --trusted-cert c815544ef4367147ab4bc564430efd72258eb2f6e1d634503c2f48c7b77da544 \
-      -u mpreuss \
-      -p $(${gopass}/bin/gopass show -o websites/id.wobcom.de/marvin.preuss@wobcom.de)
+      	vpn.wobcom.de \
+      	--trusted-cert 7a3f29e18c303c26080671cd1c0925ba2ae7c229c50eef6222d6f1453596e88d \
+      	--trusted-cert c815544ef4367147ab4bc564430efd72258eb2f6e1d634503c2f48c7b77da544 \
+      	-u mpreuss \
+      	-p $(${gopass}/bin/gopass show -o websites/id.wobcom.de/marvin.preuss@wobcom.de)
     '')
 
     # ssh
@@ -126,8 +126,8 @@ in
     attic
     (writeShellScriptBin "attic-push-store" ''
       set -euo pipefail
-      ${attic}/bin/attic push --ignore-upstream-cache-filter iot $(ls -d /nix/store/*/|grep armv5tel)
-      ${attic}/bin/attic push --ignore-upstream-cache-filter iot $(ls -d /nix/store/*/|grep chirpstack)
+      ${attic}/bin/attic push --ignore-upstream-cache-filter iot $(ls -d /nix/store/*/ | grep armv5tel)
+      ${attic}/bin/attic push --ignore-upstream-cache-filter iot $(ls -d /nix/store/*/ | grep chirpstack)
     '')
   ];
 
