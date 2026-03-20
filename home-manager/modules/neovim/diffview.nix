41d6a410d8ceac76016e4c85aeaf57ba6e16ac8a
Marvin Preuss <marvin@xsteadfastx.org>
2026-03-20 08:31:00 +0100

    feat(vim): diffview default options

--- home-manager/modules/neovim/diffview.nix
+++ home-manager/modules/neovim/diffview.nix
@@ -17,6 +17,17 @@ in
     extraLuaConfig =
       #lua
       ''
+        require("diffview").setup({
+        	default_args = {
+        		DiffviewOpen = { "--imply-local" },
+        	},
+        	hooks = {
+        		diff_buf_read = function(bufnr)
+        			vim.opt_local.foldenable = false
+        		end,
+        	},
+        })
+
         vim.keymap.set("n", "<Leader>do", ":DiffviewOpen<CR>")
         vim.keymap.set("n", "<Leader>dc", ":DiffviewClose<CR>")
       '';
