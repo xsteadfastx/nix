2aa1a357eb6c307a30de7114c73e6d22bc3908f8
Marvin Preuss <marvin@xsteadfastx.org>
2026-05-26 10:29:56 +0200

    feat(phil): configure CUPS for remote printing via Tailscale
    - Allow all connections (Location blocks with Allow all)
    - Set DefaultShared Yes so remote IPP clients can submit jobs
    - Delete cupsd.conf on each start to ensure NixOS config is applied
    - Add Brother HL-L2340D printer declaratively

--- hosts/phil/cups.nix
+++ hosts/phil/cups.nix
@@ -1,21 +1,47 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  systemd.services.cups.serviceConfig.ExecStartPre = lib.mkBefore [
+    "${pkgs.coreutils}/bin/rm -f /var/lib/cups/cupsd.conf"
+  ];
   services.printing = {
     enable = true;
-    listenAddresses = [
-      "0.0.0.0:631"
-    ];
-    allowFrom = [ "@LOCAL" ];
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
     browsing = true;
     drivers = with pkgs; [
       brlaser
     ];
+    extraConf = ''
+      DefaultShared Yes
+
+      <Location />
+        Order allow,deny
+        Allow all
+      </Location>
+      <Location /admin>
+        Order allow,deny
+        Allow all
+      </Location>
+    '';
   };
   networking.firewall.allowedTCPPorts = [ 631 ];
   networking.firewall.allowedUDPPorts = [
     631
     5353
   ];
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother-HL-L2340D";
+        location = "Phil";
+        deviceUri = "usb://Brother/HL-L2340D%20series?serial=E73870M5N464062";
+        model = "drv:///brlaser.drv/brl2340d.ppd";
+        ppdOptions.PageSize = "A4";
+      }
+    ];
+    ensureDefaultPrinter = "Brother-HL-L2340D";
+  };
+
   services.avahi = {
     enable = true;
     nssmdns4 = true;
