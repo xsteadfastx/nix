499045ce06e4c71aa2d0c9341bf0f4c46f8791ce
Marvin Preuss <marvin@xsteadfastx.org>
2026-05-26 12:49:06 +0200

    feat(coltrane): add Brother HL-1110 USB printer as default
    Add locally connected Brother HL-1110 via USB as the default printer.
    Also rename HL-L2340D to reflect its location (Villa Anna).

--- hosts/coltrane/print.nix
+++ hosts/coltrane/print.nix
@@ -12,13 +12,19 @@
   hardware.printers = {
     ensurePrinters = [
       {
-        name = "Brother-HL-L2340D";
+        name = "Brother-HL-1110-USB";
+        deviceUri = "usb://Brother/HL-1110%20series?serial=K3N519570";
+        model = "drv:///brlaser.drv/br1110.ppd";
+        ppdOptions.PageSize = "A4";
+      }
+      {
+        name = "Brother-HL-L2340D-VILLA-ANNA";
         deviceUri = "ipp://100.64.218.104:631/printers/Brother-HL-L2340D";
         model = "everywhere";
         ppdOptions.PageSize = "A4";
       }
     ];
-    ensureDefaultPrinter = "Brother-HL-L2340D";
+    ensureDefaultPrinter = "Brother-HL-1110-USB";
   };
 
   services.avahi = {
