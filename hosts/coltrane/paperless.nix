64fb2ebaad83bb2ed6f4c13ae9f835af24013b02
Marvin Preuss <marvin@xsteadfastx.org>
2026-05-26 12:49:00 +0200

    feat(coltrane): configure paperless mail polling and date extraction
    Enable mail consumer cron (every 10min) and set DMY date order for
    correct extraction of dates from German invoices.

--- hosts/coltrane/paperless.nix
+++ hosts/coltrane/paperless.nix
@@ -12,6 +12,8 @@
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
       PAPERLESS_TIME_ZONE = "Europe/Berlin";
       PAPERLESS_URL = "http://127.0.0.1:28981";
+      PAPERLESS_EMAIL_TASK_CRON = "*/10 * * * *";
+      PAPERLESS_DATE_ORDER = "DMY";
     };
     passwordFile = config.sops.secrets."paperless-admin-password".path;
     # environmentFile = config.sops.secrets."paperless-env".path;
