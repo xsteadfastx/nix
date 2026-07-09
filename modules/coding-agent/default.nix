54b61ce0117f9c2d58520bb0cfd00090e81c71ff
Marvin Preuss <marvin@xsteadfastx.org>
2026-07-09 10:44:16 +0200

    chore(coding-agent): changes default model

--- modules/coding-agent/default.nix
+++ modules/coding-agent/default.nix
@@ -547,7 +547,7 @@ in
       description = "General agent settings";
       default = {
         defaultProvider = "ollama-wobcom";
-        defaultModel = "ornith:35b";
+        defaultModel = "gemma4:31b";
         theme = "dracula";
         skills = [ "/home/marv/.claude/skills" ];
       };
