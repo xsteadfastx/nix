a6f7a157dd151404c9f6e594012964bd6d0f875e
Marvin Preuss <marvin@xsteadfastx.org>
2026-08-11 08:09:46 +0200

    chore: switch pre-commit-nix to public xsteadfastx mirror
    The private git.wobcom.de/smartmetering repo is replaced by the public
    git.xsfx.dev/xsteadfastx/pre-commit-nix mirror so the flake builds
    without the internal SSH host.

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    pre-commit.url = "git+ssh://git@git.wobcom.de/smartmetering/pre-commit-nix.git";
+    pre-commit.url = "git+https://git.xsfx.dev/xsteadfastx/pre-commit-nix.git";
     quickemu.inputs.nixpkgs.follows = "nixpkgs";
     quickemu.url = "github:quickemu-project/quickemu";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
